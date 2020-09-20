81a3c4e951a368f60567a3738eafdb2877d93364
Anuj Kalia <anujkaliaiitd@gmail.com>
2020-09-20 13:47:39 -0700

    Import from Google App Engine

---
+++ copy.sh
@@ -0,0 +1,9 @@
+# Pig and choose
+
+scp -r *.html akalia@linux.gp.cs.cmu.edu:www/
+#scp -r doc/nsdi19/erpc_nsdi_talk.pdf akalia@linux.gp.cs.cmu.edu:www/doc/nsdi19/erpc_nsdi_talk.pdf
+#scp -r doc/materials/* akalia@linux.gp.cs.cmu.edu:www/doc/materials/
+#scp -r *.css akalia@linux.gp.cs.cmu.edu:www/
+#scp -r doc/personal/resume.pdf akalia@linux.gp.cs.cmu.edu:www/doc/personal/resume.pdf
+
+#scp -r files/akalia_2018.jpg akalia@linux.gp.cs.cmu.edu:www/files/akalia_2018.jpg
